bcb7b5ad6753f8937f965161dfd4a970a7ae92db
Ali-awesome <mohammadsanaullahrabby@gmail.com>
2024-06-07 23:32:08 +0600

    simple blog crud done

--- blogModule15/blogModule15/views.py
+++ blogModule15/blogModule15/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from posts.models import Post
 def home(request):
-    return render(request, 'base.html')
+    posts = Post.objects.all()
+    return render(request, 'home.html', {'posts' : posts})
--- blogModule15/posts/views.py
+++ blogModule15/posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .forms import PostForm
+from .models import Post
 
 # Create your views here.
 def add_post(request):
@@ -11,3 +12,20 @@ def add_post(request):
     else:
         postForm = PostForm()
     return render(request, 'post.html', {'form' : postForm})
+
+def edit_post(request, id):
+    post = Post.objects.get(pk = id)
+    postForm = PostForm(instance=post)
+    if request.method == 'POST':
+        postForm = PostForm(request.POST, instance=post)
+        if postForm.is_valid():
+            postForm.save()
+            return redirect('home')
+    # else:
+    #     postForm = PostForm()
+    return render(request, 'post.html', {'form' : postForm})
+
+def delete_post(request,id):
+    post = Post.objects.get(pk = id)
+    post.delete()
+    return redirect('home')
